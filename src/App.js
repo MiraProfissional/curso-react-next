992583d68fc6935c19809765bb7475b6c5441d11
MiraProfissional <jvitor.gmira@gmail.com>
2024-03-27 16:40:34 -0300

    Componentes de classe com estado (stateful

--- src/App.js
+++ src/App.js
@@ -1,6 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
+class App extends Component {
+
+  state = {
+      name: 'Joao Vitor Mira',
+      counter: 0
+  };
+
+  handleClickP = () => {
+    this.setState({name: 'Junior'})
+  };
+
+  handleClickLink = (e) => {
+    e.preventDefault();
+    const {counter} = this.state;
+    this.setState({counter: counter + 1});
+  }
+
+  render () {
+
+    const {name,counter} = this.state
+
+    return (
+      <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p onClick={this.handleClickP}>
+          {name} {counter}
+        
+        </p>
+        <a
+          onClick={this.handleClickLink}
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+          >
+          Este e o Link
+        </a>
+      </header>
+    </div>
+  )
+}
+
+}
+
+/*
 function App() {
   return (
     <div className="App">
@@ -21,5 +68,6 @@ function App() {
     </div>
   );
 }
+*/
 
 export default App;
