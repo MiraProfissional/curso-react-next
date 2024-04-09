41092ee4df6c250ea16e4fb55e9c1f4a329faecc
MiraProfissional <jvitor.gmira@gmail.com>
2024-04-09 20:11:58 -0300

    Testando a busca por mais posts

--- src/pages/Home/Home.test.jsx
+++ src/pages/Home/Home.test.jsx
@@ -1,35 +1,10 @@
-import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
-import { Home } from '.';
-
-describe('<Home />', () => {
-  it('should render search, posts and load more', async () => {
-    render(<Home />);
-
-    const noMorePosts = screen.getByText('Nao existem posts');
-
-    await waitForElementToBeRemoved(noMorePosts);
-
-    const search = screen.getByPlaceholderText(/type your search/i);
-    expect(search).toBeInTheDocument();
-
-    //Teste com os posts estaticamente definidos como 2 na primeira renderizacao do componente. Uma solucao otima para esse problema seria receber por propos a varivel "postsPerPage" da Home.
-    // const images = screen.getAllByRole('img', { name: /title/i });
-    // expect(images).toHaveLength(2);
-
-    const button = screen.getByRole('button', { name: /More posts/i });
-    expect(button).toBeInTheDocument();
-
-    expect.assertions(2);
-  });
-});
-
 // Tentativa de utlizacao do Mock Service Web na aula dada pelo curso. Porem, eu e mais outros alunos do curso estamos tendo problemas de importacao.
 
 // import { http } from 'msw';
 // import { setupServer } from 'msw/node';
-
-// import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
-// import { Home } from '.';
+import userEvent from '@testing-library/user-event';
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import { Home } from '.';
 
 // export const handlers = [
 //   http.get('*jsonplaceholder.typicode.com/*', async (req, res, ctx) => {
@@ -63,21 +38,66 @@ describe('<Home />', () => {
 
 // const server = setupServer(...handlers);
 
-// describe('<Home />', () => {
-//   beforeAll(() => {
-//     server.listen();
-//   });
+describe('<Home />', () => {
+  // beforeAll(() => {
+  //     server.listen();
+  //   });
 
-//   afterEach(() => server.resetHandlers());
+  //   afterEach(() => server.resetHandlers());
 
-//   afterAll(() => {
-//     server.close();
-//   });
+  //   afterAll(() => {
+  //     server.close();
+  //   });
 
-//   it('should render search, posts and load more', async () => {
-//     render(<Home />);
-//     const noMorePosts = screen.getByText('Nao existem posts');
-//     await waitForElementToBeRemoved(noMorePosts);
-//     screen.debug();
-//   });
-// });
+  it('should render search, posts and load more', async () => {
+    render(<Home />);
+
+    const noMorePosts = screen.getByText('Nao existem posts');
+
+    await waitForElementToBeRemoved(noMorePosts);
+
+    const search = screen.getByPlaceholderText(/type your search/i);
+    expect(search).toBeInTheDocument();
+
+    //Teste com os posts estaticamente definidos como 2 na primeira renderizacao do componente. Uma solucao otima para esse problema seria receber por propos a varivel "postsPerPage" da Home.
+    // const images = screen.getAllByRole('img', { name: /title/i });
+    // expect(images).toHaveLength(2);
+
+    const button = screen.getByRole('button', { name: /More posts/i });
+    expect(button).toBeInTheDocument();
+
+    expect.assertions(2);
+  });
+
+  // it('should search for posts', async () => {
+  //   render(<Home />);
+
+  //   const noMorePosts = screen.getByText('Nao existem posts');
+
+  //   await waitForElementToBeRemoved(noMorePosts);
+
+  //   const search = screen.getByPlaceholderText(/type your search/i);
+  //   expect(search).toBeInTheDocument();
+
+  //   expect(screen.getByRole('heading', { name: 'title1' })).toBeInTheDocument();
+  //   expect(screen.getByRole('heading', { name: 'title2' })).toBeInTheDocument();
+  //   expect(screen.queryByRole('heading', { name: 'title3' })).not.toBeInTheDocument();
+
+  //   userEvent.type(search, 'title1');
+
+  //   expect(screen.getByRole('heading', { name: 'title1' })).toBeInTheDocument();
+  //   expect(screen.getByRole('heading', { name: 'title2' })).not.toBeInTheDocument();
+  //   expect(screen.queryByRole('heading', { name: 'title3' })).not.toBeInTheDocument();
+
+  //   expect(screen.queryByRole('heading', { name: 'Type your search' })).toBeInTheDocument();
+
+  //   userEvent.clear(search);
+
+  //   expect(screen.getByRole('heading', { name: 'title1' })).toBeInTheDocument();
+  //   expect(screen.getByRole('heading', { name: 'title2' })).toBeInTheDocument();
+
+  //   userEvent.type(search, 'Test if search for somethin that does not exists');
+
+  //   expect(screen.getByText('Nao existem posts')).toBeInTheDocument();
+  // });
+});
